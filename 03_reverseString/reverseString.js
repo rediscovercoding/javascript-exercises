064684686e5c79a5ad1787ba510e895ad5690443
kd <koustubhdube@outlook.com>
2022-06-05 15:20:38 +0200

    Solution to reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let array1 = string.split('');
+let array2 = [];
+let reversecount = array1.length-1;
+for (let i= 0; i < array1.length; i++){
+    array2[i] = array1[reversecount];
+    reversecount--;
+}
+return array2.join("");
 };
 
 // Do not edit below this line
