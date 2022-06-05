2474606dbe45316335f0bd92159a6bccb3342077
kd <koustubhdube@outlook.com>
2022-06-05 11:19:24 +0200

    02_repeatString backup

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+let newString ="";
+const repeatString = function(word,repeatNo) {
+let newString ="";
+for(let i = 1; i < (repeatNo + 1); i++){
+   newString += word 
+}
+return newString
 };
 
 // Do not edit below this line
