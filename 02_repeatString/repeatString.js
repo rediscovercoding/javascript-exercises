bda3e6692ea1f52718ed89d94c10f832c030946a
kd <koustubhdube@outlook.com>
2022-06-05 13:49:26 +0200

    Complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,19 @@
 let newString ="";
+
 const repeatString = function(word,repeatNo) {
 let newString ="";
+if (repeatNo > -1){
 for(let i = 1; i < (repeatNo + 1); i++){
    newString += word 
 }
 return newString
+}
+else {
+    return 'ERROR'
+}
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
