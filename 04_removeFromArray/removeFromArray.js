cf8997d0a389df738171eca91d9731d827660a72
kd <koustubhdube@outlook.com>
2022-06-06 16:01:32 +0200

    Solution to removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array1,num1,num2,num3,num4) {
+    let array2 =[];
+    let i = 0;
+    for(const element of array1){
+    if(array1[i] !== num1 && array1[i] !==num2 && array1[i] !==num3 && array1[i] !==num4){
+        array2.push(element);}
+    i++;
+    }
+    return array2;
 };
 
 // Do not edit below this line
