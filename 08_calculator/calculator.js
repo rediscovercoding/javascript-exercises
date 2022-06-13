61cf82c91653c4875023ec58bca4cb92095b1533
kd <koustubhdube@outlook.com>
2022-06-13 20:45:23 +0200

    solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1+num2;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2
 };
 
-const sum = function() {
-	
+const sum = function(array1) {
+	let sum = 0;
+  for (element in array1){
+    sum = array1[element] + sum;
+  };
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array1) {
+  let multiplication = 1;
+  for (element in array1){
+    multiplication = array1[element] * multiplication;
+  };
+  return multiplication;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num1) {
+  let num2 = num1;
+  if (num1 ==0 || num1 ===1)
+    return 1;
+  while(num1 > 1){
+    num1--;
+    num2 = num1* num2;
+      }
+	return num2;
+  }; 
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string1) {
+    let string2 = string1.replace(/[.! ,]/g, "");
+    let string3 = string2.toUpperCase();
+    const array1 = string3.split("");
+    
+    n = array1.length-1;
+    let boolean = null;
+    for (let i =0 ; i <array1.length; i ++){
+        
+        if(array1[i]==array1[n]){
+        boolean = true}
+        else {return false}
+    n--; 
+    }
+    return boolean;
+    }
 
 // Do not edit below this line
 module.exports = palindromes;
